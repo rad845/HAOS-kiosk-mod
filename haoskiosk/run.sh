63bf3104a067c197bd17ed70ff55d7679ccd5cce
rad845 <radek84@gmail.com>
2026-04-20 11:21:35 +0200

    Update run.sh

--- haoskiosk/run.sh
+++ haoskiosk/run.sh
@@ -623,21 +623,19 @@ fi
 
 #### Start browser (or debug mode)  and wait/sleep
 if [ "$DEBUG_MODE" != true ]; then
-    ### Run browser in the background and wait for process to exit
+    ### Run browser in the background
     $BROWSER ${BROWSER_FLAGS:+$BROWSER_FLAGS} "$HA_URL/$HA_DASHBOARD" &
     bashio::log.info "Launching $BROWSER browser(PID=$!): $HA_URL/$HA_DASHBOARD"
 
-    count=0
-    while true; do  # Wait for all browser processes to exit
-        if pgrep -f -- "^$BROWSER " > /dev/null 2>&1; then
-            count=0
-        else
-            count=$((count + 1))
+    ### Uproszczona pętla podtrzymująca (nie pozwoli dodatkowi wyjść)
+    while true; do
+        # Jeśli proces zniknie, spróbuj go zrestartować zamiast wychodzić
+        if ! pgrep -x "chromium-browser" > /dev/null; then
+            bashio::log.warning "Chromium process lost, restarting..."
+            $BROWSER ${BROWSER_FLAGS:+$BROWSER_FLAGS} "$HA_URL/$HA_DASHBOARD" &
         fi
-        [ $count -ge 3 ] && break # Exit if no browser process for at least 2*5=10 seconds
-        sleep 5
+        sleep 30
     done
-    bashio::log.info "No $BROWSER instances remaining... exiting 'run.sh'..."
 
 else  ### Debug mode
     bashio::log.info "Entering debug mode (X & $WINMGR window manager but no $BROWSER browser)..."
