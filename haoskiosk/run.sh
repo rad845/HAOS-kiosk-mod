7d2fb5a32b735220522a08a5ab9eeaf4d8b15593
rad845 <radek84@gmail.com>
2026-04-20 11:24:40 +0200

    Update run.sh

--- haoskiosk/run.sh
+++ haoskiosk/run.sh
@@ -82,7 +82,7 @@ trap cleanup HUP INT QUIT ABRT TERM EXIT
 
 ################################################################################
 BROWSER="chromium-browser"
-BROWSER_FLAGS="--kiosk --no-sandbox --user-data-dir=/data/browser --disable-gpu --disable-software-rasterizer --no-first-run"
+BROWSER_FLAGS="--kiosk --no-sandbox --user-data-dir=/data/browser --disable-dev-shm-usage --no-first-run"
 ################################################################################
 #### Get config variables from HA add-on & set environment variables
 load_config_var() {
